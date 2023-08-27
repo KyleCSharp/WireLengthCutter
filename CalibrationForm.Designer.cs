96d982d51d16e8ab13435655e1caac4fd9ff9afc
Kyle Rigler <kylelrigler@gmail.com>
2023-08-27 17:27:46 -0400

    adding in final toches

--- CalibrationForm.Designer.cs
+++ CalibrationForm.Designer.cs
@@ -28,16 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CalibrationForm));
             this.button1 = new System.Windows.Forms.Button();
             this.totalNotchesLabel = new System.Windows.Forms.Label();
             this.Backbutton = new System.Windows.Forms.Button();
             this.calibrationFormTextBox = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(646, 395);
+            this.button1.Location = new System.Drawing.Point(321, 377);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(66, 37);
             this.button1.TabIndex = 1;
@@ -48,16 +49,16 @@
             // totalNotchesLabel
             // 
             this.totalNotchesLabel.AutoSize = true;
-            this.totalNotchesLabel.Location = new System.Drawing.Point(94, 49);
+            this.totalNotchesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalNotchesLabel.Location = new System.Drawing.Point(354, 93);
             this.totalNotchesLabel.Name = "totalNotchesLabel";
-            this.totalNotchesLabel.Size = new System.Drawing.Size(426, 13);
+            this.totalNotchesLabel.Size = new System.Drawing.Size(68, 73);
             this.totalNotchesLabel.TabIndex = 2;
-            this.totalNotchesLabel.Text = "please move the wheel one inch then click ok to get the total notches moved in th" +
-    "at inch";
+            this.totalNotchesLabel.Text = "0";
             // 
             // Backbutton
             // 
-            this.Backbutton.Location = new System.Drawing.Point(722, 395);
+            this.Backbutton.Location = new System.Drawing.Point(393, 377);
             this.Backbutton.Name = "Backbutton";
             this.Backbutton.Size = new System.Drawing.Size(66, 37);
             this.Backbutton.TabIndex = 3;
@@ -67,19 +68,20 @@
             // 
             // calibrationFormTextBox
             // 
-            this.calibrationFormTextBox.Location = new System.Drawing.Point(646, 360);
+            this.calibrationFormTextBox.Location = new System.Drawing.Point(321, 351);
             this.calibrationFormTextBox.Name = "calibrationFormTextBox";
             this.calibrationFormTextBox.Size = new System.Drawing.Size(142, 20);
             this.calibrationFormTextBox.TabIndex = 4;
             // 
-            // label1
+            // label2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(571, 332);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(217, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "please enter number given in text box below ";
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(103, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(651, 144);
+            this.label2.TabIndex = 6;
+            this.label2.Text = resources.GetString("label2.Text");
             // 
             // CalibrationForm
             // 
@@ -87,7 +89,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkRed;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.calibrationFormTextBox);
             this.Controls.Add(this.Backbutton);
             this.Controls.Add(this.totalNotchesLabel);
@@ -106,6 +108,6 @@
         private System.Windows.Forms.Label totalNotchesLabel;
         private System.Windows.Forms.Button Backbutton;
         private System.Windows.Forms.TextBox calibrationFormTextBox;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
